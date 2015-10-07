a4ebb296c7d952daf977eb33fb6fb65eb130c2a5
Garrett S.Y. Hampton <G@GarrettHampton.com>
2015-10-07 11:02:17 -0700

    Added CRUD test
    Added a test for Create and Delete operation.

---
+++ src/person/crud_test.go
@@ -0,0 +1,27 @@
+package person
+
+import (
+    "testing"
+)
+
+var p = Person{
+    FirstName: "Bob",
+    LastName: "Smith",
+    EmailAddress: "notarealaddress@fakermcfakepants.com",
+    Phone: "555-555-5555",
+    Weight: 180,
+}
+
+func TestCreate(t *testing.T) {
+    t.Log("Attempting to create a new person")
+    if err := p.Create(); err != nil {
+        t.Errorf("Failed to create a new person!  %s", err)
+    }
+}
+
+func TestDelete(t *testing.T) {
+    t.Log("Attempting to delete an existing person")
+    if err := p.Delete(); err != nil {
+        t.Errorf("Failed to delete an existing person!  %s", err)
+    }
+}
