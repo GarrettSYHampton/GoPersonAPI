aea80fc0b239ba906b875f3a5839256e36ad4de1
Garrett S.Y. Hampton <G@GarrettHampton.com>
2015-10-07 12:46:51 -0700

    Moved all "person" package files into one file.

--- src/person/person.go
+++ src/person/person.go
@@ -1,5 +1,26 @@
 package person
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+	"strconv"
+)
+
+var (
+	db   *sql.DB
+	Conf Config
+)
+
+type Config struct {
+	User string `json:"user"`
+	Pass string `json:"pass"`
+	Host string `json:"host"`
+	Port int    `json:"port"`
+	DB   string `json:"db"`
+}
+
 type Person struct {
 	ID           int64  `json:"id"`
 	FirstName    string `json:"firstname"`
@@ -7,4 +28,77 @@ type Person struct {
 	EmailAddress string `json:"emailaddress"`
 	Phone        string `json:"phone"`
 	Weight       int    `json:"weight"`
+}
+
+func init() {
+
+	port, err := strconv.Atoi(os.Getenv("MYSQLPORT"))
+	if err != nil {
+		panic(err)
+	}
+
+	Conf = Config{
+		User: os.Getenv("MYSQLUSER"),
+		Pass: os.Getenv("MYSQLPASS"),
+		Host: os.Getenv("MYSQLHOST"),
+		Port: port,
+		DB:   os.Getenv("MYSQLDB"),
+	}
+
+	d := fmt.Sprintf("%s:%s@tcp4(%s:%s)/%s",
+		Conf.User,
+		Conf.Pass,
+		Conf.Host,
+		strconv.Itoa(Conf.Port),
+		Conf.DB,
+	)
+	// Attempt to establish a new SQL driver connection (does not actually connect to the SQL server)
+	db, err = sql.Open("mysql", d)
+	if err != nil {
+		panic(err)
+	}
+	// Ping does not open a connection, will only validate DSN data
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+}
+
+func (p *Person) Create() error {
+	result, err := db.Exec(`
+    		INSERT INTO person (
+    			firstname,
+    			lastname,
+    			emailaddress,
+    			phone,
+    			weight
+    		)
+    		VALUES (
+    			?, ?, ?, ?, ?
+    		)`,
+		p.FirstName, p.LastName, p.EmailAddress, p.Phone, p.Weight)
+
+	if err != nil {
+		return err
+	}
+
+	newID, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	p.ID = newID
+	return nil
+}
+
+func (p Person) Delete() error {
+	_, err := db.Exec(`
+		DELETE
+        FROM person
+        WHERE
+        id = ?
+    `, p.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- src/server/main.go
+++ src/server/main.go
@@ -9,6 +9,32 @@ import (
 )
 
 func main() {
+
+	if os.Getenv("MYSQLUSER") == "" {
+		panic("No \"MYSQLUSER\" environment variable has been configured")
+	}
+
+	if os.Getenv("MYSQLHOST") == "" {
+		panic("No \"MYSQLHOST\" environment variable has been configured")
+	}
+
+	if os.Getenv("MYSQLPORT") == "" {
+		panic("No \"MYSQLPORT\" environment variable has been configured")
+	}
+	var err error
+	var port int
+	port, err = strconv.Atoi(os.Getenv("MYSQLPORT"))
+	if err != nil {
+		panic("Environment variable \"MYSQLPORT\" needs to be an integer")
+	}
+	if os.Getenv("MYSQLDB") == "" {
+		panic("No \"MYSQLDB\" environment variable has been configured")
+	}
+
+	if port == 0 {
+		panic("Nope")
+	}
+
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", config.Host, strconv.Itoa(config.Port)), router))
 }
