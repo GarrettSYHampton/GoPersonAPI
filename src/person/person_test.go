afd219a038f3b536b00cba695c1e28e7b9f75f66
Garrett S.Y. Hampton <G@GarrettHampton.com>
2015-10-08 08:46:09 -0700

    Fixed tests for person
    Moved delete/create into “CRUD” test.  Tests are not guaranteed to run
    in any particular order, and it is best if tests do not share variables.

--- src/person/person_test.go
+++ src/person/person_test.go
@@ -6,15 +6,7 @@ import (
 	"testing"
 )
 
-var p = Person{
-	FirstName:    "Bob",
-	LastName:     "Smith",
-	EmailAddress: "notarealaddress@fakermcfakepants.com",
-	Phone:        "555-555-5555",
-	Weight:       180,
-}
-
-func TestCreate(t *testing.T) {
+func TestCRUD(t *testing.T) {
 	var err error
 	var port int
 	port, err = strconv.Atoi(os.Getenv("MYSQLPORT"))
@@ -26,26 +18,19 @@ func TestCreate(t *testing.T) {
 	if err = Connect(os.Getenv("MYSQLUSER"), os.Getenv("MYSQLPASS"), os.Getenv("MYSQLHOST"), port, os.Getenv("MYSQLDB")); err != nil {
 		t.Errorf("Error connecting to MySQL Database!  %s", err)
 		return
+	}
+
+	p := Person{
+		FirstName:    "Bob",
+		LastName:     "Smith",
+		EmailAddress: "notarealaddress@fakermcfakepants.com",
+		Phone:        "555-555-5555",
+		Weight:       180,
 	}
 
 	t.Log("Attempting to create a new person")
 	if err = p.Create(); err != nil {
 		t.Errorf("Failed to create a new person!  %s", err)
-	}
-}
-
-func TestDelete(t *testing.T) {
-	var err error
-	var port int
-	port, err = strconv.Atoi(os.Getenv("MYSQLPORT"))
-	if err != nil {
-		t.Errorf("Could not determine MySQL port from environment variables! %s", err)
-		return
-	}
-
-	if err = Connect(os.Getenv("MYSQLUSER"), os.Getenv("MYSQLPASS"), os.Getenv("MYSQLHOST"), port, os.Getenv("MYSQLDB")); err != nil {
-		t.Errorf("Error connecting to MySQL Database!  %s", err)
-		return
 	}
 
 	t.Log("Attempting to delete an existing person")
