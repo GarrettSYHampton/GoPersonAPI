c4e0b736f68c3666d1770f0b065403e956473d11
Garrett S.Y. Hampton <G@GarrettHampton.com>
2015-10-12 11:38:02 -0700

    Code refactor

--- src/server/main.go
+++ src/server/main.go
@@ -14,23 +14,18 @@ func main() {
 	r := []string {"MYSQLUSER","MYSQLHOST","MYSQLPORT","MYSQLDB"}
 	for i := range r {
 		if os.Getenv(r[i]) == "" {
-			log.Fatal("No \""+ r[i] +"\" environment variable has been configured")
-			os.Exit(1)
+			log.Fatalf("No \"%s\" environment variable has been configured", r[i])
 		}
 	}
 
-	var err error
-	var port int
-	port, err = strconv.Atoi(os.Getenv("MYSQLPORT"))
+	port, err := strconv.Atoi(os.Getenv("MYSQLPORT"))
 	if err != nil {
 		log.Fatal("Environment variable \"MYSQLPORT\" needs to be an integer")
-		os.Exit(1)
 	}
 
-	err := person.Connect(os.Getenv("MYSQLUSER"), os.Getenv("MYSQLPASS"), os.Getenv("MYSQLHOST"), port, os.Getenv("MYSQLDB"))
+	err = person.Connect(os.Getenv("MYSQLUSER"), os.Getenv("MYSQLPASS"), os.Getenv("MYSQLHOST"), port, os.Getenv("MYSQLDB"))
 	if err != nil {
 		log.Fatal("Could not connect to database")
-		os.Exit(1)
 	}
 
 	router := NewRouter()
