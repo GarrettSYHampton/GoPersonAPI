74fd3fa8dba7d4d8a566b25915ed72559fa100e6
Garrett S.Y. Hampton <G@GarrettHampton.com>
2015-10-12 11:27:36 -0700

    Refactored required env var check

--- src/server/main.go
+++ src/server/main.go
@@ -11,31 +11,28 @@ import (
 
 func main() {
 
-	if os.Getenv("MYSQLUSER") == "" {
-		panic("No \"MYSQLUSER\" environment variable has been configured")
+	r := []string {"MYSQLUSER","MYSQLHOST","MYSQLPORT","MYSQLDB"}
+	for i := range r {
+		if os.Getenv(r[i]) == "" {
+			log.Fatal("No \""+ r[i] +"\" environment variable has been configured")
+			os.Exit(1)
+		}
 	}
 
-	if os.Getenv("MYSQLHOST") == "" {
-		panic("No \"MYSQLHOST\" environment variable has been configured")
-	}
-
-	if os.Getenv("MYSQLPORT") == "" {
-		panic("No \"MYSQLPORT\" environment variable has been configured")
-	}
 	var err error
 	var port int
 	port, err = strconv.Atoi(os.Getenv("MYSQLPORT"))
 	if err != nil {
-		panic("Environment variable \"MYSQLPORT\" needs to be an integer")
-	}
-	if os.Getenv("MYSQLDB") == "" {
-		panic("No \"MYSQLDB\" environment variable has been configured")
+		log.Fatal("Environment variable \"MYSQLPORT\" needs to be an integer")
+		os.Exit(1)
 	}
 
 	err := person.Connect(os.Getenv("MYSQLUSER"), os.Getenv("MYSQLPASS"), os.Getenv("MYSQLHOST"), port, os.Getenv("MYSQLDB"))
 	if err != nil {
-		panic("Could not connect to database")
+		log.Fatal("Could not connect to database")
+		os.Exit(1)
 	}
+
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", config.Host, strconv.Itoa(config.Port)), router))
 }
